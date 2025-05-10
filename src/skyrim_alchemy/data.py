a6835d75577f45d873e5e1df5b003b1578542013
mihnea-mihai <mihnea.mihai@gmx.com>
2025-05-10 12:04:30 +0300

    new pages

--- src/skyrim_alchemy/data.py
+++ src/skyrim_alchemy/data.py
@@ -7,6 +7,10 @@ from dataclasses import dataclass, field, fields
 from functools import cache, cached_property
 from typing import Any
 
+from statistics import median
+
+from skyrim_alchemy.utils import value_formula
+
 with open("alchemy.log", "w", encoding="utf-8"):
     pass
 
@@ -18,7 +22,14 @@ logging.basicConfig(
 )
 
 
-ignorable = {"repr": False, "compare": False}
+def IGNORABLE():
+    # pylint: disable=invalid-field-call
+    return field(repr=False, compare=False)
+
+
+def COMPUTED():
+    # pylint: disable=invalid-field-call
+    return field(repr=False, compare=False, init=False)
 
 
 def coerce(value, type_):
@@ -28,20 +39,42 @@ def coerce(value, type_):
         case "int":
             return int(value)
         case "bool":
-            return int(value)
+            return value == "True"
     return value
+
+
+def coerce_fields(obj: Any):
+    for fld in fields(obj):
+        val = getattr(obj, fld.name, None)
+        if val and fld.type in ["float", "bool", "int"]:
+            setattr(obj, fld.name, coerce(val, fld.type))
 
 
 @dataclass(unsafe_hash=True)
 class Ingredient:
-    name: str
-    value: int = field(**ignorable)
-    plantable: bool = field(**ignorable)
-    vendor_rarity: str = field(**ignorable)
-    unique_to: str = field(**ignorable)
+    name: str = field(compare=True)
+    value: int = IGNORABLE()
+    plantable: bool = IGNORABLE()
+    vendor_rarity: str = IGNORABLE()
+    unique_to: str = IGNORABLE()
+
+    @cached_property
+    def traits(self) -> set[Trait]:
+        return {trait for trait in Data.traits if trait.ingredient == self}
+
+    @cached_property
+    def potencies(self) -> set[Potency]:
+        return {trait.potency for trait in self.traits}
+
+    @cached_property
+    def effects(self) -> set[Effect]:
+        return {potency.effect for potency in self.potencies}
 
     def __str__(self):
         return f"Ingredient({self.name})"
+
+    def __post_init__(self):
+        coerce_fields(self)
 
     @classmethod
     def create(cls, row: dict) -> Ingredient:
@@ -62,13 +95,31 @@ class Ingredient:
 @dataclass(unsafe_hash=True)
 class Effect:
     name: str
-    effect_type: str = field(**ignorable)
-    base_cost: float = field(**ignorable)
+    effect_type: str = IGNORABLE()
+    base_cost: float = IGNORABLE()
+
+    @cached_property
+    def traits(self) -> set[Trait]:
+        return {trait for trait in Data.traits if trait.potency.effect == self}
+
+    @cached_property
+    def potencies(self) -> set[Potency]:
+        return {trait.potency for trait in self.traits}
+
+    @cached_property
+    def median_magnitude(self) -> float:
+        return median(trait.potency.magnitude for trait in self.traits)
+
+    @cached_property
+    def median_duration(self) -> float:
+        return median(trait.potency.duration for trait in self.traits)
+
+    @cached_property
+    def median_price(self) -> float:
+        return median(trait.potency.price for trait in self.traits)
 
     def __post_init__(self):
-        for fld in fields(self):
-            if fld.type in ["float", "bool", "int"]:
-                setattr(self, fld.name, coerce(getattr(self, fld.name), fld.type))
+        coerce_fields(self)
 
     def __str__(self):
         return f"Effect({self.name})"
@@ -93,6 +144,7 @@ class Effect:
 class Trait:
     ingredient: Ingredient
     potency: Potency
+    order: int
 
     def __str__(self):
         return f"Trait({self.ingredient},{self.potency})"
@@ -102,6 +154,7 @@ class Trait:
         trait = Trait(
             Ingredient.get(row["ingredient_name"]),
             Potency.create(row["effect_name"], row["magnitude"], row["duration"]),
+            row["order"],
         )
         logger.info("Created %s", trait)
         Data.traits.add(trait)
@@ -118,14 +171,27 @@ class Trait:
                 return trait
 
 
-@dataclass(frozen=True)
+@dataclass(unsafe_hash=True)
 class Potency:
     effect: Effect
     magnitude: float
     duration: int
+    price: float = COMPUTED()
+
+    @cached_property
+    def ingredients(self) -> set[Ingredient]:
+        return {
+            trait.ingredient for trait in self.effect.traits if trait.potency == self
+        }
 
     def __str__(self):
         return f"Potency({self.effect},{self.magnitude},{self.duration})"
+
+    def __post_init__(self):
+        coerce_fields(self)
+        self.price = (
+            value_formula(self.magnitude, self.duration) * self.effect.base_cost
+        )
 
     @classmethod
     @cache  # this ensures no duplicates
@@ -134,6 +200,13 @@ class Potency:
         logger.info("Created %s", potency)
         Data.potencies.add(potency)
         return potency
+
+    @classmethod
+    @cache
+    def get(cls, ingredient: Ingredient, effect: Effect) -> Potency:
+        for potency in effect.potencies:
+            if ingredient in potency.ingredients:
+                return potency
 
 
 class Data:
@@ -164,5 +237,6 @@ class Data:
 
 if __name__ == "__main__":
     Data.read_csvs()
-    eff = Effect.get("Fear")
-    print(type(eff.base_cost))
+    for ing in Data.ingredients:
+        print(ing.plantable)
+    # print(Ingredient.__init__.__code__.co_varnames)
--- src/skyrim_alchemy/formatting.py
+++ src/skyrim_alchemy/formatting.py
@@ -1,5 +1,5 @@
-from skyrim_alchemy.data import Ingredient
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 from skyrim_alchemy.data import Data
 
 env = Environment(
@@ -9,19 +9,26 @@ env = Environment(
     lstrip_blocks=True,
     extensions=["jinja2.ext.debug"],
 )
+
+
+def crop_number(val):
+    if int(val) == val:
+        return int(val)
+    return round(val, 1)
+
+
+env.filters["crop_number"] = crop_number
 # ing = Ingredient.get("Canis Root")
-# # with open("docs/index.html", "w", encoding="utf-8") as file_out:
-# #     file_out.write(env.get_template("default.html.jinja").render(title=""))
+with open("docs/index.html", "w", encoding="utf-8") as file_out:
+    file_out.write(env.get_template("default.html.jinja").render(title=""))
 
 # tpl = env.get_template("formatting/ingredient/plantable.html.jinja")
 
 # print(tpl.render(ingredient=ing))
 
+
 Data.read_csvs()
-for page in [
-    "effects",
-    #   "ingredients", "potencies", "traits"
-]:
+for page in ["effects", "ingredients", "potencies", "traits"]:
     with open(f"docs/{page}.html", "w", encoding="utf-8") as file_out:
         file_out.write(
             env.get_template(f"{page}.html.jinja").render(
